50a024ab0c62e5f51c32a544b5cec921299f479b
Sittichai Seangnon <148406690+SSmikey@users.noreply.github.com>
2024-09-02 13:02:10 +0700

    setup two

--- webpage/urls.py
+++ webpage/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.indexPage, name="index"),
+    path("details/", views.detailsPage, name="details"),
+    path("browse/", views.browsePage, name="browse"),
+    path("profile/", views.profilePage, name="profile"),
+    path("streams/", views.streamsPage , name="streams")
 ]
--- webpage/views.py
+++ webpage/views.py
@@ -4,4 +4,16 @@ from django.shortcuts import render, HttpResponse
 
 
 def indexPage(request):
-    return render(request, 'base.html')
+    return render(request, 'index.html')
+
+def detailsPage(request):
+    return render(request, 'details.html')
+
+def browsePage(request):
+    return render(request, 'browse.html')
+
+def profilePage(request):
+    return render(request, 'profile.html')
+
+def streamsPage(request):
+    return render(request, 'streams.html')
