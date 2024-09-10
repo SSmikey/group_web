a392a0fdccb8174916abaee6cc87e93ed5057b54
Sittichai Seangnon <148406690+SSmikey@users.noreply.github.com>
2024-09-10 09:56:20 +0700

    static

--- group_web/settings.py
+++ group_web/settings.py
@@ -117,10 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-
-STATICFILES_DIRS = [BASE_DIR / 'statics', ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'statics')
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'statics')]
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles_build', 'statics')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
